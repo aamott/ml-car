0093798660d67d1a6b5f80a01a92b863cf2eaec0
aamott <adam.amott@gmail.com>
2022-02-10 18:08:59 -0700

    Added car test

---
+++ pi_car_test.py
@@ -0,0 +1,17 @@
+from pi_car import PiCar
+from time import sleep
+
+car = PiCar()
+
+car.set_steering(0.5)
+sleep(1)
+car.set_steering(-0.5)
+sleep(1)
+
+car.set_speed(1)
+sleep(1)
+
+car.set_speed(-1)
+sleep(1)
+car.set_speed(0)
+sleep(1)
