32d9a885376fe4b52f7b6840a1403a93fc9e2a3c
aamott <adam.amott@gmail.com>
2022-03-17 21:25:56 -0600

    Added camera-server for testing

---
+++ remote-car/video-streamer/camera-server.sh
@@ -0,0 +1,2 @@
+# start this on the raspberry pi. run './camera-server.sh'
+raspivid -t 999999 -w 1080 -h 720 -fps 25 -hf -b 2000000 -o - | gst-launch-1.0 -v fdsrc ! h264parse ! rtph264pay config-interval=1 pt=96 ! gdppay ! tcpserversink host=<IP-OF-PI> port=5000
