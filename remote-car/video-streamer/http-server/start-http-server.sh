14703ab07fd33cbb88c784bde2662cdd2a481e6d
aamott <adam.amott@gmail.com>
2022-04-21 18:25:40 -0600

    removed type

--- remote-car/video-streamer/http-server/start-http-server.sh
+++ remote-car/video-streamer/http-server/start-http-server.sh
@@ -4,4 +4,4 @@ hostname -i
 
 echo "Starting service on http://<rpi_ip>:8081"
 echo "You can view this video in a browser or connect OpenCV using the above address"
-libcamerify motion/motion
+libcamerify motion
